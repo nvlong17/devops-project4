a083085cb980ec73acfb5e87b6ae6aa4ccafa5e6
nvlong17 <nvlong17@gmail.com>
2022-12-30 01:30:29 +0700

    Task 6 + 7 + 8

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=nvlong17/udacity4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity4 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity4-66c6bccddf-jlnqh 8000:80
